9887b4f9b7180eb327e47c03e65381f8b67b75b3
Ivan Sokolov <keupoz@gmail.com>
2021-03-07 05:41:14 +0700

    Fixed pug plugin

--- src/pug.ts
+++ src/pug.ts
@@ -1,6 +1,6 @@
 import { glob } from "glob";
 import { posix } from "path";
-import { render } from "pug";
+import { compile } from "pug";
 import { initIcons } from "./fontawesome";
 import { markdown } from "./markdown";
 import { plugin } from "./plugin";
@@ -10,7 +10,7 @@ const icon = initIcons();
 export function pug(data: any, pretty: boolean) {
     return plugin("pug", (chunk, encoding, callback) => {
         const contents = chunk.contents.toString(encoding),
-            rendered = render(contents, {
+            template = compile(contents, {
                 pretty,
 
                 cache: true,
@@ -20,14 +20,13 @@ export function pug(data: any, pretty: boolean) {
                     markdown(template: string) {
                         return markdown(template, false);
                     }
-                },
+                }
+            }),
+            rendered = template({
+                data, icon, markdown,
 
-                locals: {
-                    data, icon, markdown,
-
-                    glob(pattern: string) {
-                        return glob.sync(pattern).map((path) => posix.join("/", path));
-                    }
+                glob(pattern: string) {
+                    return glob.sync(pattern).map((path) => posix.join("/", path));
                 }
             });
 
